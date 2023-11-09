bfc4309549947e169ab587ab116319e74a321680
miguel <mramirez@shokworks.io>
2023-11-09 03:36:09 -0500

    wip: add about, contact, cart function

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ carCounter.style.display =  carCounter.textContent === '0' ? 'none' : 'block';
 const p = new Products();
 const products  = await p.getProducts();
 const productsIterator = products.values();
-
 const documentProducts = document.querySelector('#products');
+
+const addToCart = (id)=> {
+    console.log("click" + id)
+
+}
 for (const p of productsIterator) {
-    // <h3>${p.name}</h3><p>${p.description}</p><p>${p.price}</p>
-    // <button class="btn btn-primary" id="${p.id}">Add to car</button>
-    documentProducts.innerHTML += `
-    <div class="col-3 my-3 text-black rounded ">
-        <div class="p-3 h-100 rounded  border ">
+    const productContainer = document.createElement('div');
+    productContainer.className = 'col-3 my-3 text-black rounded ';
+
+    productContainer.innerHTML = `
+        <div class="p-3 h-100 rounded border ">
             <div class="d-flex justify-content-between align-items-center flex-column h-100">
                 <div class="d-flex justify-content-center align-items-center flex-column h-100">
                     <img src="${p.path}" alt="${p.name}" class="img-fluid rounded shadow">
@@ -25,14 +29,20 @@ for (const p of productsIterator) {
                     <hr class="w-100">
                     <h4>${p.name}</h4>
                     <p>${p.description}</p>
-                    <p></p><strong class="text-danger">${p.price}</strong></p>
-                    <button class="btn btn-danger shadow text-white" id="${p.id}">Add to car</button>
+                    <p><strong class="text-danger">${p.price}</strong></p>
+                    <button class="btn btn-danger shadow text-white" id="${p.uuid}">
+                        <img src="./assets/svg/add-shopping-cart.svg" alt="add_cart">
+                    </button>
                 </div>
             </div>
         </div>
-    </div>`;
+    `;
+
+    // Agrega el contenedor del producto al documento
+    documentProducts.appendChild(productContainer);
+
+    // Agrega el evento de clic al botón después de agregarlo al DOM
+    const addToCartButton = productContainer.querySelector(`#${p.uuid}`);
+    addToCartButton.addEventListener('click', () => addToCart(p.uuid));
 }
-
-
-
 
